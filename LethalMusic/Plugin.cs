5ad872ca4919ba8fa813a6981aa82c409c1f9e52
CTN <info@ctn-originals.com>
2024-02-16 18:48:24 +0100

    Initial setup
    The project has been set up with all the tools needed to get started.

---
+++ LethalMusic/Plugin.cs
@@ -0,0 +1,24 @@
+using BepInEx;
+using BepInEx.Logging;
+
+namespace LethalMusic {
+	[BepInPlugin("com.github.legoandmars.lethalmusic", "LethalMusic", "0.0.1")]
+	public class Plugin : BaseUnityPlugin {
+		public static ManualLogSource CLog;
+		
+		public static string PluginGUID = "com.github.legoandmars.lethalmusic";
+		public static string PluginName = "LethalMusic";
+		public static string PluginVersion = "0.0.1";
+
+		public static bool DebugMode = false;
+
+		public void Awake() {
+			#if DEBUG
+				DebugMode = true;
+			#endif
+
+			CLog = Logger;
+			CLog.LogInfo($"Plugin {PluginName} is loaded! Version: {PluginVersion} ({(DebugMode ? "Debug" : "Release")})");
+		}
+	}
+}
